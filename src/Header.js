41d1ff35efcd47c224b06b6a34a32aabb33a2d28
Francisco Martin <BRHBS1004@ambev.com.br>
2020-08-23 19:06:45 -0300

    Header & firebase config

---
+++ src/Header.js
@@ -0,0 +1,39 @@
+import React from "react";
+import "./Header.css";
+import MenuIcon from "@material-ui/icons/Menu";
+import SearchIcon from "@material-ui/icons/Search";
+import VideoCallIcon from "@material-ui/icons/VideoCall";
+import AppsIcon from "@material-ui/icons/Apps";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+import Avatar from "@material-ui/core/Avatar";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header_left">
+        <MenuIcon />
+        <img
+          className="header_logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/e/e1/Logo_of_YouTube_%282015-2017%29.svg"
+          alt=""
+        />
+      </div>
+      <div className="header_input">
+        <input placeholder="Search" type="text" />
+        <SearchIcon className="header_inputButtom" />
+      </div>
+      <div className="header_icons">
+        <VideoCallIcon className="header_icon" />
+        <AppsIcon className="header_icon" />
+        <NotificationsIcon className="header_icon" />
+        <Avatar
+          className="avatar"
+          alt="Francisco Martin"
+          src="https://lh3.googleusercontent.com/ogw/ADGmqu9rlIUepeoEa7l8EEjfOqKF3qpT8chxWskUeLcs=s32-c-mo"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCHSVEh0VxtLS6I2HUr9U9bI9AnCuZAExY",
+    authDomain: "clone-635f1.firebaseapp.com",
+    databaseURL: "https://clone-635f1.firebaseio.com",
+    projectId: "clone-635f1",
+    storageBucket: "clone-635f1.appspot.com",
+    messagingSenderId: "950126947810",
+    appId: "1:950126947810:web:393f630438b706893a1dbb",
+    measurementId: "G-B6QXC7DYJE"
+  };
