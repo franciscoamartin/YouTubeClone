4fbffd87b08dfa4f7896d33042674708939f28eb
Francisco Martin <BRHBS1004@ambev.com.br>
2020-08-30 12:37:13 -0300

    add recommendedVideos

--- src/components/RecommendedVideos/RecommendedVideos.js
+++ src/components/RecommendedVideos/RecommendedVideos.js
@@ -1,10 +1,61 @@
 import React from "react";
-import './style/RecommendedVideos.css'
+import VideoCard from "./VideoCard/VideoCard";
+import "./style/RecommendedVideos.css";
 
 function RecommendedVideos() {
   return (
     <div className="recommendedVideos">
-      <h1>videos</h1>
+      <h2>Recommended</h2>
+      <div className="recommendedVideos_videos">
+        <VideoCard
+          title="Become a web Developer in 10 minutes | 2019/2020"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://yt3.ggpht.com/a-/AOh14GhOUP_9re93yOMdN4RLQLHl6Mbk6vmUS1524w=s68-c-k-c0x00ffffff-no-rj-mo"
+          channel="InfoTech"
+          image="https://i.ytimg.com/vi/r5iFrxi9-hY/maxresdefault.jpg"
+        />
+        <VideoCard
+          title="Become a web Developer in 10 minutes | 2019/2020"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://yt3.ggpht.com/a-/AOh14GhOUP_9re93yOMdN4RLQLHl6Mbk6vmUS1524w=s68-c-k-c0x00ffffff-no-rj-mo"
+          channel="InfoTech"
+          image="https://i.ytimg.com/vi/r5iFrxi9-hY/maxresdefault.jpg"
+        />
+        <VideoCard
+          title="Become a web Developer in 10 minutes | 2019/2020"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://yt3.ggpht.com/a-/AOh14GhOUP_9re93yOMdN4RLQLHl6Mbk6vmUS1524w=s68-c-k-c0x00ffffff-no-rj-mo"
+          channel="InfoTech"
+          image="https://i.ytimg.com/vi/r5iFrxi9-hY/maxresdefault.jpg"
+        />
+        <VideoCard
+          title="Become a web Developer in 10 minutes | 2019/2020"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://yt3.ggpht.com/a-/AOh14GhOUP_9re93yOMdN4RLQLHl6Mbk6vmUS1524w=s68-c-k-c0x00ffffff-no-rj-mo"
+          channel="InfoTech"
+          image="https://i.ytimg.com/vi/r5iFrxi9-hY/maxresdefault.jpg"
+        />
+        <VideoCard
+          title="Become a web Developer in 10 minutes | 2019/2020"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://yt3.ggpht.com/a-/AOh14GhOUP_9re93yOMdN4RLQLHl6Mbk6vmUS1524w=s68-c-k-c0x00ffffff-no-rj-mo"
+          channel="InfoTech"
+          image="https://i.ytimg.com/vi/r5iFrxi9-hY/maxresdefault.jpg"
+        />
+        <VideoCard
+          title="Become a web Developer in 10 minutes | 2019/2020"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://yt3.ggpht.com/a-/AOh14GhOUP_9re93yOMdN4RLQLHl6Mbk6vmUS1524w=s68-c-k-c0x00ffffff-no-rj-mo"
+          channel="InfoTech"
+          image="https://i.ytimg.com/vi/r5iFrxi9-hY/maxresdefault.jpg"
+        />
+      </div>
     </div>
   );
 }
